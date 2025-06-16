b7ddf64565c9634ff92b56ae79833e212d5578c6
vince85d <vince85d@gmail.com>
2025-06-15 20:41:49 -0700

    Update job_scraper.py

--- job_scraper.py
+++ job_scraper.py
@@ -14,7 +14,7 @@ class TAMUJobScraper:
         self.keywords = [
             "reptile", "amphibian", "herp", "turtle", "toad", "frog", 
             "seal", "island", "whale", "cetacean", "tortoise", 
-            "spatial ecology", "predator", "tropical", "bird"
+            "spatial ecology", "predator", "tropical", "wildlife"
         ]
         self.email_config = email_config
         self.sent_jobs_file = "sent_jobs.json"
